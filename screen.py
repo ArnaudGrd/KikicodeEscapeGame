7f0f9563d0392a6d93e884ae12dd6152ad4b4775
ArnaudGrd <arnaud.grandisson@gmail.com>
2023-12-13 14:35:35 -0500

    initial commit

---
+++ screen.py
@@ -0,0 +1,7 @@
+import tkinter
+
+fenetre = tkinter.Tk()
+label = tkinter.Label(fenetre, text="-.. .. -..- \\ ... . .--. -",font=("Courrier", 150), bg="#a3cd92")
+label.pack()
+fenetre.config(background="#a3cd92")
+fenetre.mainloop()
